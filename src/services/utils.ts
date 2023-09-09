f2e5c7a8dd92535ae4cc84160838266fadb144d0
Rafael Ross <rafaellross@gmail.com>
2023-09-09 14:25:53 -0300

    Alterado home page

--- src/services/utils.ts
+++
@@ -1,7 +0,0 @@
-const options: Intl.DateTimeFormatOptions = {
-  dateStyle: "short",
-  timeStyle: "medium"
-};
-
-export const formatDate = (date: Date) =>
-  Intl.DateTimeFormat("pt-br", options).format(date);
